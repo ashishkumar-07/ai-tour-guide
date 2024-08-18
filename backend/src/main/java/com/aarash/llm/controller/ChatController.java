d57ea865d720f084bf8452a7c6e8d07b4b78a1f5
User <kumar@gmail.com>
2024-08-18 11:00:09 +0530

    modified code as new snapshot relase

--- backend/src/main/java/com/aarash/llm/controller/ChatController.java
+++ backend/src/main/java/com/aarash/llm/controller/ChatController.java
@@ -1,10 +1,6 @@
 package com.aarash.llm.controller;
 
-import java.util.Map;
-
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.client.ChatClient;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +13,26 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/chat")
 public class ChatController {
 
-    private final OllamaChatClient chatClient;
+    private final ChatClient chatClient;
 
-    public ChatController(OllamaChatClient chatClient) {
-        this.chatClient = chatClient;
+    public ChatController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
     }
 
-    @GetMapping("/generate")
-    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me about city London") String message) {
-        return Map.of("generation", chatClient.call(message));
+    @GetMapping()
+    public String generate(@RequestParam(defaultValue = "Tell me a joke.") String message) {
+        return chatClient.prompt().user(message).call().content();
     }
 
-    @GetMapping("/generate-stream")
+    @GetMapping("/stream")
 	public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "Tell me about city London") String message) {
-
-        Prompt prompt = new Prompt(new UserMessage(message));
-        return chatClient.stream(prompt).map(m->m.getResult().getOutput().getContent());
+        return chatClient.prompt()
+                .system("""
+                            You are an helpful AI tour guide. You can provide the historical
+                            information about cities under 500 character
+                            """)
+                .user(message)
+                .stream()
+                .content();
     }
 }
--- backend/src/main/java/com/aarash/llm/controller/CityController.java
+++ backend/src/main/java/com/aarash/llm/controller/CityController.java
@@ -1,7 +1,7 @@
 package com.aarash.llm.controller;
 
 import com.aarash.llm.model.CityInfo;
-import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
@@ -20,13 +20,8 @@ import java.util.Map;
 public class CityController {
     private final ChatClient llmClient;
 
-    public CityController(ChatClient llmClient) {
-        this.llmClient = llmClient;
-    }
-
-    @GetMapping("/{city}")
-    public CityInfo cityInfo(@PathVariable String city) {
-        String systemMessagePrompt = """
+    public CityController(ChatClient.Builder chatClientBuilder) {
+        String defaultSystemPrompt = """
                 You are an helpful AI tour guide. You can provide information about cities to the users who want to travel to those cities. Your response should include following information:
                 1. Small description about the city
                 2. History of the city
@@ -38,15 +33,19 @@ public class CityController {
                 8. Getting around in the city
                 9. More information about the city which includes web links for more information
                 """;
+        this.llmClient = chatClientBuilder.defaultSystem(defaultSystemPrompt).build();
+    }
+
+    @GetMapping("/{city}")
+    public CityInfo cityInfo(@PathVariable String city) {
+
         String userMessagePrompt = """
             Tell me about {city}. If You do not know the city then you must not provide any information.
-            {format}
         """;
-        Message systemMessage = new SystemPromptTemplate(systemMessagePrompt).createMessage();
-        var parser = new BeanOutputParser<CityInfo>(CityInfo.class);
-        Message userMessage = new PromptTemplate(userMessagePrompt).createMessage(Map.of("city", city, "format", parser.getFormat()));
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-
-        return parser.parse(llmClient.call(prompt).getResult().getOutput().getContent());
+        CityInfo cityInfo = llmClient.prompt()
+                .user(u->u.text(userMessagePrompt).param("city", city))
+                .call()
+                .entity(CityInfo.class);
+        return cityInfo;
     }
 }
